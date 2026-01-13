3db54baec10c109e35b5a0bc932aa5102ef88bff
abrokecube <58220690+abrokecube@users.noreply.github.com>
2026-01-12 23:33:29 -0600

    :)

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def main():
     """
     # The monitor thread is already started when importing 'app' from 'api'
     # because of the module-level code in api.py.
-    uvicorn.run(app, host="127.0.0.1", port=7102)
+    uvicorn.run(app, host="0.0.0.0", port=7102)
 
 if __name__ == "__main__":
     main()
